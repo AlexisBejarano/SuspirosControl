e3d0dbc99ea9404865065b93d71db4feb8d1fbf7
Alexis Bejarano <aletzisbejaranov@hotmail.com>
2025-03-18 16:15:52 -0700

    Se instalo Axios y Universal-cookie, este ultimo es para guardar las variables de inicio de sesion

--- src/login.jsx
+++ src/login.jsx
@@ -5,7 +5,7 @@ export default function Login() {
         <>
             <div className="max-w-sm space-y-3 m-auto">
                 <div className="relative">
-                    <input type="email" className="peer py-2.5 sm:py-3 px-4 ps-11 block w-full bg-gray-100 border-transparent rounded-lg sm:text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none" placeholder="Enter name"/>
+                    <input type="text" className="peer py-2.5 sm:py-3 px-4 ps-11 block w-full bg-gray-100 border-transparent rounded-lg sm:text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none" placeholder="Enter name"/>
                         <div className="absolute inset-y-0 start-0 flex items-center pointer-events-none ps-4 peer-disabled:opacity-50 peer-disabled:pointer-events-none">
                             <svg className="shrink-0 size-4 text-gray-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                                 <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path>
