ac413a728456c594b6b71d63f5ed11c245e2b331
Alexis Bejarano <aletzisbejaranov@hotmail.com>
2025-05-03 00:27:39 -0700

    Se restablecio variable (letra) eliminada por error

--- src/App.jsx
+++ src/App.jsx
@@ -400,7 +400,7 @@ const TableComponent = () => {
         </div>
       )}
 
-      {(sexportLoading || dataLoading) && (
+      {(exportLoading || dataLoading) && (
         <div className="fixed inset-0 flex items-center justify-center backdrop-blur-xs bg-opacity-30 z-30">
           <div className="bg-white p-6 rounded-lg shadow-xl flex flex-col items-center">
             <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-500 mb-4"></div>
